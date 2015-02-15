c4c2a91fa47141bddb76120b7d1ddfb3f44ed45a
mafitov <maksim.afitov@gmail.com>
2015-02-15 13:36:52 +0300

    Add idea update

--- client/app/idea/idea.controller.js
+++ client/app/idea/idea.controller.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('ideaApp')
-  .controller('IdeaCtrl', function ($scope, $stateParams, $http, socket, Idea, Auth) {
-    
+  .controller('IdeaCtrl', function($scope, $stateParams, $http, socket, Idea, Auth) {
+
     $scope.isAdmin = Auth.isAdmin;
-    
+    $scope.alerts = [];
+
     Idea.getIdea().success(function(idea) {
       $scope.idea = idea;
       $scope.isOwner = function() {
@@ -12,9 +13,30 @@ angular.module('ideaApp')
       };
       socket.syncUpdates('idea', $scope.idea);
     });
-    
-    $scope.$on('$destroy', function () {
+
+    $scope.update = function(form) {
+      $scope.submitted = true;
+      if (form.$valid) {
+        Idea.updateIdea($scope.idea).success(function() {
+          $scope.alerts.push({
+            type: 'success',
+            msg: 'Idea successfully updated.'
+          });
+        }).error(function(data) {
+          $scope.alerts.push({
+            type: 'danger',
+            msg: data
+          });
+        });
+      }
+    }
+
+    $scope.$on('$destroy', function() {
       socket.unsyncUpdates('idea');
     });
     
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+
   });
