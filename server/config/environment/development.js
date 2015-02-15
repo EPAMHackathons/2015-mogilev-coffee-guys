cad8a78a6d195a02535c457d6257fc33e4a80b1e
mafitov <maksim.afitov@gmail.com>
2015-02-15 09:50:52 +0300

    Initial commit. Yeoman generated.

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://idea:idea@ds037611.mongolab.com:37611/idea'
+  },
+
+  seedDB: true
+};
