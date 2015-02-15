9a96bbe2f82cf074de213e348b84af2dfc12156c
mafitov <maksim.afitov@gmail.com>
2015-02-15 15:47:00 +0300

    Fix collapsed nav menu

--- client/components/partials/navbar-primary/navbar-primary.controller.js
+++ client/components/partials/navbar-primary/navbar-primary.controller.js
@@ -3,6 +3,7 @@
 angular.module('ideaApp')
   .controller('NavbarPrimaryCtrl', function ($scope, Auth, $location) {
 
+    $scope.isCollapsed = true;
     $scope.getCurrentUser = Auth.getCurrentUser;
     $scope.isAdmin = Auth.isAdmin;
     $scope.isLoggedIn = Auth.isLoggedIn;
